e66510dd6afaf49d720192a0604074d0e8f726b4
ncksllvn <ncksllvn@gmail.com>
2020-05-24 15:50:10 -0400

    Add toggle for selected piece

--- src/reducers/ui.js
+++ src/reducers/ui.js
@@ -10,6 +10,13 @@ export default function userInteractions(state = initialState, action) {
   switch (action.type) {
     case PIECE_SELECTED: {
       const selectedPiece = action.piece
+      if (
+        selectedPiece.file === state.selectedPiece?.file &&
+        selectedPiece.rank === state.selectedPiece?.rank
+      ) {
+        return { ...state, selectedPiece: null }
+      }
+
       return { ...state, selectedPiece }
     }
 
