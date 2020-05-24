f0294c625b4249202f9b98f58ae26765a80ea2ac
ncksllvn <ncksllvn@gmail.com>
2020-05-24 11:47:43 -0400

    Convert to hooks

--- src/components/Piece.jsx
+++ src/components/Piece.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { connect } from 'react-redux'
 
 import {
   PIECE_SYMBOLS,
@@ -15,49 +14,36 @@ const {
   BLACK
 } = KEYS
 
-class Piece extends React.Component {
-  render() {
-    const {
-      file,
-      rank,
-      type,
-      typeName,
-      colorName,
-      moves
-    } = this.props
+export default function Piece({ file, rank, type, color }) {
+  const {
+    piecesBySymbol,
+    colorsBySymbol
+  } = useSelector(state => state.constants)
 
-    if (!type) {
-      return null
-    }
+  const {
+    movesBySquare
+  } = useSelector(state => state.chess)
 
-    const className = `chess-board-piece${
-      colorName === BLACK ? ' chess-board-piece--black' : ''
-    }`
-
-    return (
-      <button
-        data-square={`${file}${rank}`}
-        disabled={moves?.length === 0}
-        onClick={() => console.log(moves)}
-        className={className}
-        aria-label={`${colorName} ${typeName} on ${file.toUpperCase()} ${rank}`}>
-        {PIECE_SYMBOLS[typeName][colorName]}
-      </button>
-    )
+  if (!type) {
+    return null
   }
-}
 
-const mapStateToProps = (state, { type, color, file, rank }) => {
-  return {
-    moves: state.chess.movesBySquare[`${file}${rank}`],
-    typeName: state.constants.piecesBySymbol[type],
-    colorName: state.constants.colorsBySymbol[color]
-  }
-}
+  const moves = movesBySquare[`${file}${rank}`]
+  const typeName = piecesBySymbol[type]
+  const colorName = colorsBySymbol[color]
 
-const mapDispatchToProps = {
-  pieceSelected
-}
+  const className = `chess-board-piece${
+    colorName === BLACK ? ' chess-board-piece--black' : ''
+  }`
 
-export default connect(mapStateToProps, mapDispatchToProps)(Piece)
-export { Piece }
+  return (
+    <button
+      data-square={`${file}${rank}`}
+      disabled={moves?.length === 0}
+      onClick={() => console.log(moves)}
+      className={className}
+      aria-label={`${colorName} ${typeName} on ${file.toUpperCase()} ${rank}`}>
+      {PIECE_SYMBOLS[typeName][colorName]}
+    </button>
+  )
+}
