2065c133ebad17dc526034a89996b140010d7db0
ncksllvn <ncksllvn@gmail.com>
2020-05-27 22:12:20 -0400

    Updates

---
+++ precache-manifest.aac38a5d735abbbd5757333d228ad5ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b251f64108cb5d1b30801e56c22948b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e2c9fc2a2f5ff1feaf0",
+    "url": "/static/css/main.1992f739.chunk.css"
+  },
+  {
+    "revision": "3dcecb0007e6ff7cb520",
+    "url": "/static/js/2.273383de.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.273383de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e2c9fc2a2f5ff1feaf0",
+    "url": "/static/js/main.ed3e3540.chunk.js"
+  },
+  {
+    "revision": "cf9a18a95cc44a104bd3",
+    "url": "/static/js/runtime-main.a207c6aa.js"
+  }
+]);
