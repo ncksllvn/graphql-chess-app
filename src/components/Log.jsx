d8ad43b0985a4d559a29de182eabb6c812c0858a
ncksllvn <ncksllvn@gmail.com>
2020-05-31 18:49:17 -0400

    Scroll log via React effect

--- src/components/Log.jsx
+++ src/components/Log.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
 import { useSelector } from 'react-redux'
 
 // https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/ARIA_Techniques/Using_the_log_role
 export default function Log(){
   const gameLog = useSelector(state => state.gameLog)
+  const containerEl = useRef(null)
+
+  useEffect(() => {
+    containerEl.current.scrollTop = containerEl.current.scrollHeight
+  }, [gameLog])
 
   return (
-    <div className="console" role="log">
+    <div ref={containerEl} className="console" role="log">
       <ul className="console-output" aria-live="polite" aria-atomic="false">
         {gameLog?.map((gameEvent, index) =>
           <li key={index} className="console-output-item">{gameEvent}</li>
